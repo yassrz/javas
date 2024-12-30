44648ba3d38ac4ef3010959e9ab7391c7aa55286
yassrz <yasirarafatmakak75@gmail.com>
2024-12-30 09:53:59 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
   public static void main(String[] args){
-    System.out.println("hello world");
+    //vairiables ~
+    int age = 19;
+    int myAge = 20; 
+    float herage = 19.2F;
+    long views = 3_567_89_34L;
+    char letter = 'D';
+    boolean isGay = false;
+      System.out.println("hello world");
+      System.out.println(age);
+      System.out.println(views);
+      System.out.println(herage);
+      System.out.println(letter);
+      System.out.println(isGay);
   }
 }
